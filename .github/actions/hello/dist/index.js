43475c1ee58e8ebee61713a4c3cd5774e62f7fdf
LPMendoza <lpedromendoza333@gmail.com>
2022-09-14 16:45:12 -0500

    fix: asd

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -3888,15 +3888,23 @@ const core = __webpack_require__(440);
 const github = __webpack_require__(864);
 
 try {
-  throw (new Error('some error message'));
-  
+  // throw(new Error('some error message'));
+  core.debug('Debug message');
+  core.warning('Warning message');
+  core.error('Error message');
+
   const name = core.getInput('who-to-greet');
+  core.setSecret(name);
   console.log(`Hello ${name}`);
   
   const time = new Date();
   core.setOutput('time', time.toTimeString());
   
+  core.startGroup('Loggin github object');
   console.log(JSON.stringify(github, null, '\t'));
+  core.endGroup();
+
+  core.exportVariable('HELLO', 'hello');
 } catch (error) {
   core.setFailed(error.message);
 }
